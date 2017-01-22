bd89c36aa34f700c20cdb17f3eef968a0d1c524d
bea <bianca.subion@gmail.com>
2017-01-22 14:50:09 -0800

    clean up helpers

--- helpers.py
+++ helpers.py
@@ -1,17 +1,13 @@
 import math
 
 def hasConflict(row, column, num, board):
-	columnConflict = False
-	rowConflict = False
-	quadrantConflict = False
-
 	for i in range(0,9):
 		if board[row][i] == num:
-			rowConflict = True
+			return True
 
 	for j in range(0,9):
 		if board[j][column] == num:
-			columnConflict = True
+			return True
 
         ro = int(math.floor(row / 3) * 3)
         col = int(math.floor(column / 3) * 3)
@@ -19,14 +15,11 @@ def hasConflict(row, column, num, board):
 	for k in range(ro,ro + 3):
 		for l in range(col, col + 3):
 			if board[k][l] == num:
-				quadrantConflict = True
+				return True
 				
 
 
-	if columnConflict or rowConflict or quadrantConflict:
-		return True
-	else:
-		return False
+	return False
 		
 def generateEmptyBoard():
 	return [[0 for i in range(9)]for j in range(9)]
